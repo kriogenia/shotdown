c244a6b7a80ec0f7e05191a641b28a5b08985b46
Ricardo Soto Estévez <uo265710@uniovi.es>
2020-12-31 17:31:19 +0100

    Add vehicle to MenuLayer

--- Shotdown/MenuLayer.cpp
+++ Shotdown/MenuLayer.cpp
@@ -3,6 +3,12 @@
 MenuLayer::MenuLayer(Game* game) :
 	Layer(game)
 {
+	/* Add vehicle filenames */
+	vehicles.push_back("res/backgrounds/v-police.png");
+	vehicles.push_back("res/backgrounds/v-red.png");
+	vehicles.push_back("res/backgrounds/v-truck.png");
+	vehicles.push_back("res/backgrounds/v-yellow.png");
+	/* Init */
 	init();
 }
 
@@ -11,17 +17,24 @@ void MenuLayer::init()
 	background = new Actor(ActorType::BACKGROUND, "res/backgrounds/menu.png",
 		WIDTH / 2, HEIGHT / 2, WIDTH, HEIGHT, game);
 	buildings = new Parallax("res/backgrounds/near-buildings.png", WIDTH, HEIGHT, game);
-
+	generateVehicle();
+	newVehicle = VEHICLE_SPAWN_TIME;
 }
 
 void MenuLayer::tick()
 {
-
+	vehicle->position.x -= VEHICLE_SPEED;
+	newVehicle--;
+	if (newVehicle <= 0) {
+		newVehicle = VEHICLE_SPAWN_TIME;
+		generateVehicle();
+	}
 }
 
 void MenuLayer::render()
 {
 	background->render();
+	vehicle->render();
 	buildings->render();
 }
 
@@ -38,4 +51,11 @@ void MenuLayer::mouseToControl(SDL_Event event)
 void MenuLayer::padToControl(SDL_Event event)
 {
 
+}
+
+void MenuLayer::generateVehicle()
+{
+	string file = vehicles[rand() % vehicles.size()];
+	float y = rand() % 400 + 40;
+	vehicle =  new Actor(ActorType::BACKGROUND, file, WIDTH + 61, y, 96, 61, game);
 }
--- Shotdown/MenuLayer.h
+++ Shotdown/MenuLayer.h
@@ -3,6 +3,9 @@
 #include "Layer.h"
 #include "Parallax.h"
 
+constexpr auto VEHICLE_SPAWN_TIME = 300;    // 5s
+constexpr auto VEHICLE_SPEED = 6;
+
 class MenuLayer :
     public Layer
 {
@@ -18,8 +21,14 @@ public:
     void padToControl(SDL_Event event) override;
 
 private:
+    /* Background */
     Actor* background = nullptr;
     Parallax* buildings = nullptr;
+    /* Vehicles */
+    Actor* vehicle = nullptr;
+    vector<string> vehicles;
+    void generateVehicle();
+    int newVehicle = VEHICLE_SPAWN_TIME;
 
 };
 
